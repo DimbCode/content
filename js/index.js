144361fbb7a85353cfd7dfcc2f7e96a2617abb8d
Vyacheslav2123 <72310551+Vyacheslav2123@users.noreply.github.com>
2022-03-15 23:25:03 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,29 +1,27 @@
 const sliderWrap = document.querySelector(".slaider-flex");
-const slider = document.querySelector(".slaider__img");
-const slides = document.querySelectorAll(".slaider__img");
 let slideStyle = getComputedStyle(slider);
 let slideWrapStyle = getComputedStyle(sliderWrap);
 let index = 0;
 let max = slides.length;
 
 window.onload = window.onresize = () => {
-	sliderWrap.style.transitionDuration = "0s";
-	sliderWrap.offsetHeight;
-	sliderWrap.style.width = "100%";
-	let currentWidth = sliderWrap.clientWidth;
-	sliderWrap.style.transitionDuration = ".5s";
-	sliderWrap.offsetHeight;
-	let elsFull = Math.floor(currentWidth / (slides[0].scrollWidth + 25));
-	max = slides.length - elsFull;
-	sliderWrap.style.width = (elsFull * (slides[0].scrollWidth + 25) - 25) + "px";
+ sliderWrap.style.transitionDuration = "0s";
+ sliderWrap.offsetHeight;
+ sliderWrap.style.width = "100%";
+ let currentWidth = sliderWrap.clientWidth;
+ sliderWrap.style.transitionDuration = ".5s";
+ sliderWrap.offsetHeight;
+ let elsFull = Math.floor(currentWidth / (slides[0].scrollWidth + 25));
+ max = slides.length - elsFull;
+ sliderWrap.style.width = (elsFull * (slides[0].scrollWidth + 25) - 25) + "px";
 }
 
 function slide(num) {
-	if(num > 0 && index >= max){
-		index = 0;
-	}else if(num > 0 || index > 0) index += num;
-	sliderWrap.scrollTo({
-		left: index * (slides[0].scrollWidth + 25), 
-		behavior: 'smooth'
-	  });
+ if(num > 0 && index >= max){
+  index = 0;
+ }else if(num > 0 || index > 0) index += num;
+ sliderWrap.scrollTo({
+  left: index * (slides[0].scrollWidth + 25), 
+  behavior: 'smooth'
+   });
 }
